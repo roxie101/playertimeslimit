9a31fe92c706fb03c41b92110af73e4897408110
roxie101 <108659579+roxie101@users.noreply.github.com>
2020-03-13 18:46:54 +0000

    Testing Japanese and DB

--- app/app.ts
+++ app/app.ts
@@ -2,4 +2,5 @@ import Ib from '@stoqey/ib';
 import { stock } from '@stoqey/ib/dist/contract/stock';
 import WebSocket from 'ws';
 
-const w
+const wss = new WebSocket.Server({ port: 8080 });
+let con
