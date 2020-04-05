191fe21869a4c76a199305dcf9479c987b0c1e20
roxie101 <108659579+roxie101@users.noreply.github.com>
2020-04-05 17:05:11 +0000

    updating cv

--- app/app.ts
+++ app/app.ts
@@ -42,4 +42,4 @@ wss.on('connection', (ws) => {
     connection.reqHistoricalData(6002, contract, '', '3600 S', '1 min', 'ADJUSTED_LAST', 1, 1, false);
     connection.reqRealTimeBars(6003, contract, 5, 'TRADES', true);
 
-    connection.on('historicalData', (reqId: number, date: string, open: number, high: number, low: number, close: num
+    connection.on('historicalData', (reqId: number, date: string, open: number, high: number, low: number, close: number, volume: number, count: number, WAP: number, h
