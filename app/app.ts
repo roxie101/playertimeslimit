16bc8c7e69a1d7578a960f3b963fefd194e1051c
roxie101 <108659579+roxie101@users.noreply.github.com>
2020-04-18 08:32:01 +0000

    update master branch by changes in 8.0.x-dev (branch 8.0)

--- app/app.ts
+++ app/app.ts
@@ -55,4 +55,4 @@ wss.on('connection', (ws) => {
         );
     });
 
-    connection.on('realtimeBar', (reqId: number, time: number, open: number
+    connection.on('realtimeBar', (reqId: number, time: number, open: number, high: number, low: number, close: number, vol
