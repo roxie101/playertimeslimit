3d6042f7fc18eb393ebd7b1203977907d2e1ff9f
roxie101 <108659579+roxie101@users.noreply.github.com>
2020-09-28 17:50:37 +0000

    New upstream release.

--- fe/angular/ib-trading-bot/e2e/protractor.conf.js
+++ fe/angular/ib-trading-bot/e2e/protractor.conf.js
@@ -1,2 +1,4 @@
 // Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+const { SpecReporter } = r
