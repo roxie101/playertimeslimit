46b2664ce8b8ca3e05b4953893a9c7c89aeae32f
roxie101 <108659579+roxie101@users.noreply.github.com>
2020-10-28 21:03:16 +0000

    hj

--- fe/angular/ib-trading-bot/e2e/protractor.conf.js
+++ fe/angular/ib-trading-bot/e2e/protractor.conf.js
@@ -14,4 +14,6 @@ exports.config = {
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
-  jasmineNodeOpts
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval:
