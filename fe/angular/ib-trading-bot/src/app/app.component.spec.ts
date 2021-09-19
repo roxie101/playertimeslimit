b08ce62b855baadf8b617a44f1ed7f7076bc310a
roxie101 <108659579+roxie101@users.noreply.github.com>
2021-09-19 08:59:53 +0000

    release 0.9.2

--- fe/angular/ib-trading-bot/src/app/app.component.spec.ts
+++ fe/angular/ib-trading-bot/src/app/app.component.spec.ts
@@ -2,4 +2,6 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+  
