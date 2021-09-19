a4ba8faad5d4d802ae40193868ce91614980a547
roxie101 <108659579+roxie101@users.noreply.github.com>
2021-09-19 08:56:46 +0000

    Create empty.txt

--- fe/angular/ib-trading-bot/src/app/app.component.spec.ts
+++ fe/angular/ib-trading-bot/src/app/app.component.spec.ts
@@ -1,3 +1,5 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { App
+import { AppComponent } from './app.component';
+
+describe('
