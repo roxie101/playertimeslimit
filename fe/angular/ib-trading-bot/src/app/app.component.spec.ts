51cc50046c7a4e3433e4801850322636cc1363ad
roxie101 <108659579+roxie101@users.noreply.github.com>
2021-10-03 17:47:56 +0000

    add validator

--- fe/angular/ib-trading-bot/src/app/app.component.spec.ts
+++ fe/angular/ib-trading-bot/src/app/app.component.spec.ts
@@ -18,3 +18,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it(`should have as title '
