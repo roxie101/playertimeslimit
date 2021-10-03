48373deb294910d341054afaca5316ef77cab235
roxie101 <108659579+roxie101@users.noreply.github.com>
2021-10-03 12:35:05 +0000

    Upstream

--- fe/angular/ib-trading-bot/src/app/app.component.spec.ts
+++ fe/angular/ib-trading-bot/src/app/app.component.spec.ts
@@ -15,4 +15,5 @@ describe('AppComponent', () => {
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent)
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugEl
