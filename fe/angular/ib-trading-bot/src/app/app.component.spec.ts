a0058afd97f10bfb46868f8012eeef4fd805d913
roxie101 <108659579+roxie101@users.noreply.github.com>
2021-10-17 10:50:08 +0000

    Release v0.1.5

--- fe/angular/ib-trading-bot/src/app/app.component.spec.ts
+++ fe/angular/ib-trading-bot/src/app/app.component.spec.ts
@@ -29,4 +29,4 @@ describe('AppComponent', () => {
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
- 
+    const compiled = fixture.debugElement
