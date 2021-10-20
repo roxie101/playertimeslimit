cf9ae9c84f0d4cb82cf793fed26dc54c66fc9b11
roxie101 <108659579+roxie101@users.noreply.github.com>
2021-10-20 19:27:06 +0000

    Delete requirements.txt

--- fe/angular/ib-trading-bot/src/app/app.component.spec.ts
+++ fe/angular/ib-trading-bot/src/app/app.component.spec.ts
@@ -30,4 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome 
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ib-trading-bot!');
+  });
+});
