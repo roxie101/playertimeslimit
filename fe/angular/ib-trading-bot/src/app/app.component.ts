3463ca015c0b841dc7d6800de6a0dd4977681439
roxie101 <108659579+roxie101@users.noreply.github.com>
2021-10-30 14:12:58 +0000

    manager

--- fe/angular/ib-trading-bot/src/app/app.component.ts
+++ fe/angular/ib-trading-bot/src/app/app.component.ts
@@ -2,3 +2,10 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ib-trading-bot';
+}
---
+++ fe/angular/ib-trading-bot/src/app/app.module.ts
@@ -0,0 +1,1 @@
+import { BrowserModule } from '@angul
