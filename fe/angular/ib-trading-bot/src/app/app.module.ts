995bedea36c492c715ce6677cb640d44f99da155
roxie101 <108659579+roxie101@users.noreply.github.com>
2021-11-04 21:26:56 +0000

    Changed configuration

--- fe/angular/ib-trading-bot/src/app/app.module.ts
+++ fe/angular/ib-trading-bot/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './ap
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } 
