aea8bca90700f643fe84f04b2beee4bce6b98816
roxie101 <108659579+roxie101@users.noreply.github.com>
2021-11-07 19:34:53 +0000

    Happy New Year

--- fe/angular/ib-trading-bot/src/app/app.module.ts
+++ fe/angular/ib-trading-bot/src/app/app.module.ts
@@ -2,4 +2,5 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } 
+import { AppComponent } from './app.component';
+import { MarketDataGra
