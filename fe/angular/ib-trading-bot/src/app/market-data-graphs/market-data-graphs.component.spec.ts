e069c8ce25526c64a3ef5d8725ad811666647b4b
roxie101 <108659579+roxie101@users.noreply.github.com>
2021-12-19 21:57:57 +0000

    Update Parent POM Version

--- fe/angular/ib-trading-bot/src/app/market-data-graphs/market-data-graphs.component.spec.ts
+++ fe/angular/ib-trading-bot/src/app/market-data-graphs/market-data-graphs.component.spec.ts
@@ -1,3 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MarketDataGraphsComponent } from './market-dat
+import { MarketDataGraphsComponent } from './market-data-graphs.component';
+
+describe('MarketDataGraphsC
