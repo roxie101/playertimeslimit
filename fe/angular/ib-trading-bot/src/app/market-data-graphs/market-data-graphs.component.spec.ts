19c56e09cb48c79b222eae1c3a3c72db0430445b
roxie101 <108659579+roxie101@users.noreply.github.com>
2021-12-25 19:39:24 +0000

    Release 0.2.3

--- fe/angular/ib-trading-bot/src/app/market-data-graphs/market-data-graphs.component.spec.ts
+++ fe/angular/ib-trading-bot/src/app/market-data-graphs/market-data-graphs.component.spec.ts
@@ -3,4 +3,5 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MarketDataGraphsComponent } from './market-data-graphs.component';
 
 describe('MarketDataGraphsComponent', () => {
-  let component: MarketData
+  let component: MarketDataGraphsComponent;
+  let fixture: ComponentFixture<M
