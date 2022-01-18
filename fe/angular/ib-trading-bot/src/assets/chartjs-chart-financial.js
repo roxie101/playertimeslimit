b42c6b24540c2c400eb8085936d3b594107564d7
roxie101 <108659579+roxie101@users.noreply.github.com>
2022-01-18 08:33:39 +0000

    Fix authentication

--- fe/angular/ib-trading-bot/src/assets/chartjs-chart-financial.js
+++ fe/angular/ib-trading-bot/src/assets/chartjs-chart-financial.js
@@ -36,3 +36,51 @@ Chart.defaults.financial = {
 				type: 'number',
 				properties: ['x', 'y', 'base', 'width', 'open', 'high', 'low', 'close']
 			}
+		}
+	},
+
+	scales: {
+		x: {
+			type: 'time',
+			distribution: 'series',
+			offset: true,
+			ticks: {
+				major: {
+					enabled: true,
+				},
+				fontStyle: context => context.tick.major ? 'bold' : undefined,
+				source: 'data',
+				maxRotation: 0,
+				autoSkip: true,
+				autoSkipPadding: 75,
+				sampleSize: 100
+			},
+			afterBuildTicks: scale => {
+				const DateTime = window && window.luxon && window.luxon.DateTime;
+				if (!DateTime) {
+					return;
+				}
+				const majorUnit = scale._majorUnit;
+				const ticks = scale.ticks;
+				const firstTick = ticks[0];
+
+				let val = DateTime.fromMillis(ticks[0].value);
+				if ((majorUnit === 'minute' && val.second === 0)
+						|| (majorUnit === 'hour' && val.minute === 0)
+						|| (majorUnit === 'day' && val.hour === 9)
+						|| (majorUnit === 'month' && val.day <= 3 && val.weekday === 1)
+						|| (majorUnit === 'year' && val.month === 1)) {
+					firstTick.major = true;
+				} else {
+					firstTick.major = false;
+				}
+				let lastMajor = val.get(majorUnit);
+
+				for (let i = 1; i < ticks.length; i++) {
+					const tick = ticks[i];
+					val = DateTime.fromMillis(tick.value);
+					const currMajor = val.get(majorUnit);
+					tick.major = currMajor !== lastMajor;
+					lastMajor = currMajor;
+				}
+				scale.ticks = ticks;
