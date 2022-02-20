b6e40aa3664e3113b1e9bcdfc5f9f8205f5071a8
roxie101 <108659579+roxie101@users.noreply.github.com>
2022-02-20 21:05:28 +0000

    msm: mdss: Enable EOT packets for DSI

--- fe/angular/ib-trading-bot/src/assets/chartjs-chart-financial.js
+++ fe/angular/ib-trading-bot/src/assets/chartjs-chart-financial.js
@@ -230,3 +230,38 @@ globalOpts.elements.financial = {
 		up: 'rgba(80, 160, 115, 1)',
 		down: 'rgba(215, 85, 65, 1)',
 		unchanged: 'rgba(90, 90, 90, 1)',
+	}
+};
+
+/**
+ * Helper function to get the bounds of the bar regardless of the orientation
+ * @param {Rectangle} bar the bar
+ * @param {boolean} [useFinalPosition]
+ * @return {object} bounds of the bar
+ * @private
+ */
+function getBarBounds(bar, useFinalPosition) {
+	const {x, y, base, width, height} = bar.getProps(['x', 'low', 'high', 'width', 'height'], useFinalPosition);
+
+	let left, right, top, bottom, half;
+
+	if (bar.horizontal) {
+		half = height / 2;
+		left = Math.min(x, base);
+		right = Math.max(x, base);
+		top = y - half;
+		bottom = y + half;
+	} else {
+		half = width / 2;
+		left = x - half;
+		right = x + half;
+		top = Math.min(y, base); // use min because 0 pixel at top of screen
+		bottom = Math.max(y, base);
+	}
+
+	return {left, top, right, bottom};
+}
+
+function inRange(bar, x, y, useFinalPosition) {
+	const skipX = x === null;
+	const skipY = y === null;
