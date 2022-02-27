7fdaefd7acf8724901ef3f7597ebaa742ce9b29e
roxie101 <108659579+roxie101@users.noreply.github.com>
2022-02-27 14:00:09 +0000

    ftrace: Consistently restore trace function on sysctl enabling

--- fe/angular/ib-trading-bot/src/assets/chartjs-chart-financial.js
+++ fe/angular/ib-trading-bot/src/assets/chartjs-chart-financial.js
@@ -456,3 +456,30 @@ class OhlcController extends FinancialController {
 	updateElements(elements, start, mode) {
 		const me = this;
 		const dataset = me.getDataset();
+		const ruler = me._ruler || me._getRuler();
+
+		for (let i = 0; i < elements.length; i++) {
+			const index = start + i;
+			const options = me.resolveDataElementOptions(index, mode);
+
+			const baseProperties = me.calculateElementProperties(index, ruler, mode === 'reset', options);
+			const properties = {
+				...baseProperties,
+				datasetLabel: dataset.label || '',
+				lineWidth: dataset.lineWidth,
+				armLength: dataset.armLength,
+				armLengthRatio: dataset.armLengthRatio,
+				color: dataset.color,
+			};
+			properties.options = options;
+
+			me.updateElement(elements[i], index, properties, mode);
+		}
+	}
+
+}
+
+OhlcController.prototype.dataElementType = OhlcElement;
+Chart.controllers.ohlc = OhlcController;
+
+})));
