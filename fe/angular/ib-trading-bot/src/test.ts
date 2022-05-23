5f2d322a6a6f0ac51cce0b500c66cafec13a6644
roxie101 <108659579+roxie101@users.noreply.github.com>
2022-05-23 17:53:34 +0000

    Variablen angepasst re #1922

--- fe/angular/ib-trading-bot/src/test.ts
+++ fe/angular/ib-trading-bot/src/test.ts
@@ -1,1 +1,3 @@
-// This file is required by karma.conf.js and loads recursively all the .spe
+// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+
+import 'zone.js/dist/zon
