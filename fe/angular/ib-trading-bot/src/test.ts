c6a32f505e0d2173dba1fa64c409204e318c583a
roxie101 <108659579+roxie101@users.noreply.github.com>
2022-05-28 14:45:05 +0000

    Publishing latest changes

--- fe/angular/ib-trading-bot/src/test.ts
+++ fe/angular/ib-trading-bot/src/test.ts
@@ -5,4 +5,6 @@ import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
-} from '@angular/platform-bro
+} from '@angular/platform-browser-dynamic/testing';
+
+declare const require:
