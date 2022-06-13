5544ec527dc7a8df1e9996827270584c4f0c5cde
roxie101 <108659579+roxie101@users.noreply.github.com>
2022-06-13 13:46:46 +0000

    metas

--- fe/angular/ib-trading-bot/src/test.ts
+++ fe/angular/ib-trading-bot/src/test.ts
@@ -16,4 +16,5 @@ getTestBed().initTestEnvironment(
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
-// And load
+// And load the modules.
+context.keys().map(context);
